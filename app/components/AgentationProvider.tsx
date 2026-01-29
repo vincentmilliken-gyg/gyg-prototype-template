add9a37558f7a5fb973a910e6963665386991aaf
Vincent <vincent.milliken@getyourguide.com>
2026-01-29 10:13:44 +0100

    added Agentation component for the dev env

---
+++ app/components/AgentationProvider.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { Agentation } from "agentation";
+
+export function AgentationProvider() {
+  if (process.env.NODE_ENV !== "development") {
+    return null;
+  }
+  return <Agentation />;
+}
